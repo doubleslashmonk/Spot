a49abd7b8a7a0ee2078626d30fb35412146a3cf7
Mudassir Malik <Mudassir.malik@live.com>
2016-07-06 20:11:35 +0530

    Added office and table components

--- App/components/Office.js
+++ App/components/Office.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import TABLE_LIST from '../data/TABLE_LIST';
+import Table from './Table';
 
 const Office = () => {
-    return (
-        <div className = "office">
-           
-        </div>
-    )
-}
+		return (
+			<div className = "office">
+				{
+					TABLE_LIST.map((table) => <Table {...table}/> )
+				}
+			</div>
+		)
+	}
 
 export default Office;
---
+++ App/components/Table.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Table = ({ name, members}) => { 
+    return (
+		<div className = "table">
+			{name}
+		</div>
+    );
+}
+
+export default Table
