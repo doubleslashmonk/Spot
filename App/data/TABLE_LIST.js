e8aaa82610ac3cd81962438959982214d3f8bc61
Mudassir Malik <Mudassir.malik@live.com>
2016-07-06 20:06:15 +0530

    Added redux store and static data file

---
+++ App/data/TABLE_LIST.js
@@ -0,0 +1,155 @@
+const 
+	TABLE_LIST = [
+		{
+			name: 'Table 1',
+			members: [
+				{
+					designation: 'SR. Developer',
+					name: 'Shilpy',
+					project: 'Dell App Design',
+					seat: 1,
+					team: 'Mobile Frontend'
+				},
+				{
+					designation: 'SR. Designer',
+					name: 'Jason',
+					project: 'Apple Design',
+					seat: 3,
+					team: 'Apple'
+				},
+				{
+					designation: 'JR. Manager',
+					name: 'Alex',
+					project: 'Oracle CMS',
+					seat: 4,
+					team: 'Oracle'
+				}
+			]
+		},
+		{
+			name: 'Table 2',
+			members: [
+				{
+					designation: 'HR Relations',
+					name: 'Akriti',
+					project: '',
+					seat: 2,
+					team: 'HR'
+				},
+				{
+					designation: 'HR Lead',
+					name: 'Roy',
+					project: '',
+					seat: 4,
+					team: 'HR'
+				}
+			]
+		},
+		{
+			name: 'Table 3',
+			members: [
+				{
+					designation: 'Admin',
+					name: 'Mr. Mojo',
+					project: '',
+					seat: 1,
+					team: 'Admin'
+				},
+				{
+					designation: 'Admin Junior',
+					name: 'Guddu',
+					project: '',
+					seat: 2,
+					team: 'Admin'
+				},
+				{
+					designation: 'Cashier',
+					name: 'Tinu',
+					project: '',
+					seat: 4,
+					team: 'Admin'
+				}
+			]
+		},
+		{
+			name: 'Table 4',
+			members: [
+				{
+					designation: 'Sr. Frontend Dev',
+					name: 'Ashka',
+					project: 'Web Applications Development',
+					seat: 1,
+					team: 'App Frontend'
+				},
+				{
+					designation: 'Jr. Frontend Dev',
+					name: 'Krish Kapoor',
+					project: 'Web Applications Development',
+					seat: 2,
+					team: 'App Frontend'
+				},
+				{
+					designation: 'Sr. Backend Dev',
+					name: 'Mudassir Malik',
+					project: 'Web Applications Development',
+					seat: 3,
+					team: 'App Backend'
+				},
+				{
+					designation: 'Jr. Backend Dev',
+					name: 'Deepika',
+					project: 'Web Applications Development',
+					seat: 4,
+					team: 'App Backend'
+				}
+			]
+		},
+		{
+			name: 'Table 5',
+			members: [
+				{
+					designation: 'Sr. Frontend Dev',
+					name: 'Ashka',
+					project: 'Web Applications Development',
+					seat: 1,
+					team: 'App Frontend'
+				},
+				{
+					designation: 'Jr. Frontend Dev',
+					name: 'Krish Kapoor',
+					project: 'Web Applications Development',
+					seat: 2,
+					team: 'App Frontend'
+				},
+				{
+					designation: 'Sr. Backend Dev',
+					name: 'Mudassir Malik',
+					project: 'Web Applications Development',
+					seat: 3,
+					team: 'App Backend'
+				}
+			]
+		},
+		{
+			name: 'Table 7',
+			members: [
+				{
+					designation: 'Sr. Backend Dev',
+					name: 'Mudassir Malik',
+					project: 'Web Applications Development',
+					seat: 3,
+					team: 'App Backend'
+				},
+				{
+					designation: 'Jr. Backend Dev',
+					name: 'Deepika',
+					project: 'Web Applications Development',
+					seat: 4,
+					team: 'App Backend'
+				}
+			]
+		}
+
+	];
+
+	export default TABLE_LIST;
